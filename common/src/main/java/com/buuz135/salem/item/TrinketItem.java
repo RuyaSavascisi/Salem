7403c1a1b12e1c3ff7ba84937a8db801e6185c07
Buuz135 <buuz135@hotmail.com>
2021-10-30 12:38:30 +0200

    Added trinket tooltips

--- common/src/main/java/com/buuz135/salem/item/TrinketItem.java
+++ common/src/main/java/com/buuz135/salem/item/TrinketItem.java
@@ -3,11 +3,19 @@ package com.buuz135.salem.item;
 import com.buuz135.salem.SalemMod;
 import com.buuz135.salem.world.SalemRaidSavedData;
 import com.google.common.collect.*;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 
 public class TrinketItem extends Item {
 
@@ -16,5 +24,11 @@ public class TrinketItem extends Item {
     public TrinketItem(SalemRaidSavedData.SalemRaidTier raidTier, Rarity rarity) {
         super(new Properties().stacksTo(1).tab(SalemMod.TAB).rarity(rarity));
         TRINKETS.put(raidTier, this);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> list, TooltipFlag tooltipFlag) {
+        super.appendHoverText(itemStack, level, list, tooltipFlag);
+        list.add(new TranslatableComponent(getDescriptionId()+".tooltip").withStyle(ChatFormatting.GRAY));
     }
 }
